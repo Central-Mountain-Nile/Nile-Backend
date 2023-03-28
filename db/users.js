9b4bb328e55b600ac4c42d3dd5f518e906e78e08
ziggyboy2001 <brentpurks1@icloud.com>
2023-03-28 14:19:26 -0500

    working on users db

--- db/users.js
+++ db/users.js
@@ -14,7 +14,7 @@ async function createUser({
   state,
   country,
   postalCode,
-  CreatedAd,
+  CreatedAt,
 }) {
   //   const SALT_COUNT = 10;
   //   const hashpassword = await bcrypt.hash(password, SALT_COUNT);
@@ -24,16 +24,128 @@ async function createUser({
       rows: [users],
     } = await client.query(
       `
-        INSERT INTO users(username, password)
-        VALUES($1, $2)
+        INSERT INTO users(  
+          username,
+          password,
+          firstName,
+          lastName,
+          isActive,
+          isAdmin,
+          addressLine1,
+          addressLine2,
+          city,
+          state,
+          country,
+          postalCode,
+          CreatedAt)
+        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
         ON CONFLICT (username) DO NOTHING
         RETURNING *;
         `,
-      [username, hashpassword]
+      [
+        username,
+        password,
+        firstName,
+        lastName,
+        isActive,
+        isAdmin,
+        addressLine1,
+        addressLine2,
+        city,
+        state,
+        country,
+        postalCode,
+        CreatedAt,
+      ]
     );
-    delete user.password;
-    return user;
+    delete users.password;
+    return users;
   } catch (error) {
     throw error;
   }
 }
+async function getUsersByUsername(username) {
+  try {
+    const {
+      rows: [users],
+    } = await client.query(
+      `
+      SELECT * FROM users 
+      WHERE username = $1;
+    `,
+      [username]
+    );
+    return users;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+async function getUsersById(usersId) {
+  try {
+    const {
+      rows: [users],
+    } = await client.query(
+      `
+      SELECT * FROM users
+      WHERE id = $1;
+      `,
+      [usersId]
+    );
+    delete users.password;
+    return users;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+async function patchUsers({ usersId, ...fields }) {
+  // don't try to update the id
+  // do update the name and description
+  // return the updated activity
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+  try {
+    if (setString.length > 0) {
+      await client.query(
+        `
+            UPDATE users
+            SET ${setString}
+            WHERE id=${usersId}
+            RETURNING *;
+          `,
+        Object.values(fields)
+      );
+    }
+
+    return await getUsersById(usersId);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+async function deleteUsers(usersId) {
+  try {
+    const { rows } = await client.query(
+      `
+      DELETE FROM users
+      WHERE id = $1
+      RETURNING *;
+      `,
+      [usersId]
+    );
+
+    return rows;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+module.exports = {
+  createUser,
+  getUsersById,
+  getUsersByUsername,
+  patchUsers,
+  deleteUsers,
+};
