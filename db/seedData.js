24aef1347680cd64d69c123fe9f97e8a8ce2083b
ziggyboy2001 <brentpurks1@icloud.com>
2023-03-29 10:29:39 -0500

    working on users_payments

--- db/seedData.js
+++ db/seedData.js
@@ -1,4 +1,5 @@
 const client = require("./client");
+const { createUsers } = require("./");
 
 async function dropTables() {
   console.log("Dropping All Tables...");
@@ -45,20 +46,20 @@ async function createTables() {
     await client.query(`
         CREATE TABLE users (
         id SERIAL PRIMARY KEY,
-        first_name VARCHAR(255) NOT NULL,
-        last_name VARCHAR(255) NOT NULL,
+        firstName VARCHAR(255) NOT NULL,
+        lastName VARCHAR(255) NOT NULL,
         username varchar(255) UNIQUE NOT NULL,
         password varchar(255) NOT NULL,
-        is_active boolean DEFAULT TRUE,
-        is_admin boolean DEFAULT FALSE,
+        isActive boolean DEFAULT TRUE,
+        isAdmin boolean DEFAULT FALSE,
         email VARCHAR(255) UNIQUE NOT NULL,
-        address_line_one VARCHAR(255) NOT NULL,
-        address_line_two VARCHAR(255),
+        addressLineOne VARCHAR(255) NOT NULL,
+        addressLineTwo VARCHAR(255),
         city VARCHAR(255) NOT NULL,
         state VARCHAR(255),
         country VARCHAR(255) NOT NULL,
-        postal_code INTEGER NOT NULL,
-        created_at TIMESTAMP NOT NULL
+        postalCode INTEGER NOT NULL,
+        createdAt TIMESTAMP NOT NULL
           );
         `);
 
@@ -147,10 +148,89 @@ async function createTables() {
   }
 }
 
-async function createInitialUsers() {}
+async function createInitialUsers() {
+  console.log("Starting to create users...");
+  try {
+    const usersToCreate = [
+      {
+        firstName: "Brent",
+        lastName: "Purks",
+        username: "bPurks",
+        password: "testPass",
+        addressLineOne: "1000 Main St.",
+        addressLineTwo: "",
+        city: "Dallas",
+        state: "Texas",
+        country: "US",
+        postalCode: "75071",
+        createdAt: "12:00",
+        email: "testing@test.com",
+      },
+      {
+        firstName: "John",
+        lastName: "Jones",
+        username: "jJones",
+        password: "testPass2",
+        addressLineOne: "2222 Main St.",
+        addressLineTwo: "",
+        city: "Atlanta",
+        state: "Georgia",
+        country: "US",
+        postalCode: "30040",
+        createdAt: "12:00",
+        email: "testing@test.com",
+      },
+      {
+        firstName: "Jake",
+        lastName: "Harrison",
+        username: "tHarrison",
+        password: "testPass3",
+        addressLineOne: "3333 Main St.",
+        addressLineTwo: "",
+        city: "Mckinney",
+        state: "Texas",
+        country: "US",
+        postalCode: "75071",
+        createdAt: "12:00",
+        email: "testing@test.com",
+      },
+    ];
+    const users = await Promise.all(usersToCreate.map(createUsers));
+
+    console.log("Users created:");
+    console.log(users);
+    console.log("Finished creating users!");
+  } catch (error) {
+    console.error("Error creating users!");
+    throw error;
+  }
+}
 async function createCategories() {}
 async function createInitialProducts() {}
 async function createInitialDiscounts() {}
 async function createInitialCarts() {}
 async function createInitialPayments() {}
 async function createInitialOrderHistory() {}
+
+async function rebuildDB() {
+  try {
+    await dropTables();
+    await createTables();
+    await createInitialUsers();
+    await createCategories();
+    await createInitialProducts();
+    await createInitialDiscounts();
+    await createInitialCarts();
+    await createInitialPayments();
+    await createInitialOrderHistory();
+  } catch (error) {
+    console.log("Error during rebuildDB");
+    throw error;
+  }
+}
+
+module.exports = {
+  rebuildDB,
+  dropTables,
+  createTables,
+};
