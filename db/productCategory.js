f05d130030b705a81147a0825b0c1525b128a53e
ziggyboy2001 <brentpurks1@icloud.com>
2023-03-29 13:04:47 -0500

    finished products & product categories

--- db/productCategory.js
+++ db/productCategory.js
@@ -1,21 +1,39 @@
-const client = require("./client")
+const client = require("./client");
 
-async function createCategories ({name}) {
-    try {
-        const {
-            rows: [product_category]
-        } = await client.query(`
+async function createCategories({ name }) {
+  try {
+    const {
+      rows: [product_category],
+    } = await client.query(
+      `
             INSERT INTO product_category (name)
             VALUES($1)
             ON CONFLICT (name) DO NOTHING
             RETURNING *;
-            `, [name]);
-        return product_category;
-    } catch (error) {
-        throw error;
-    }
+            `,
+      [name]
+    );
+    return product_category;
+  } catch (error) {
+    throw error;
+  }
 }
 
-module.exports = {
-    createCategories
+async function getAllCategories() {
+  try {
+    const { rows } = await client.query(
+      `
+        SELECT *
+        FROM product_category;
+        `
+    );
+    return rows;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
+module.exports = {
+  createCategories,
+  getAllCategories,
+};
