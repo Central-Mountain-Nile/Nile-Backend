36b552b580e3c7e50a695c96d7e093a7b4f21d78
dylanstewart06 <dylanstewart06@gmail.com>
2023-03-29 07:59:08 -0600

    wokring on createCategories function

--- db/productCategory.js
+++ db/productCategory.js
@@ -1,10 +1,26 @@
 const client = require("./client")
 
-async function productCategory ({ category }) {
-    const {rows} = await client.query(
-        `
-        INSERT INTO product
-        
-        `
-    )
+async function createCategories({
+       category
+      }) {
+
+        try {
+            const {
+                rows: [product_category],
+            } = await client.query(
+                `
+               INSERT INTO product_category(
+               VALUES($1),
+               ON CONFLICT (category) DO NOTHING,
+               RETURNING *;
+                `
+            );
+        } catch (error) {
+            throw error;
+        }
+      }
+
+
+module.exports = {
+    createCategories
 }
