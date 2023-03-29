ce5458cb3d43230c5e0306c50d720c26939d0b44
ziggyboy2001 <brentpurks1@icloud.com>
2023-03-29 13:23:20 -0500

    finished discounts

---
+++ db/discounts.js
@@ -0,0 +1,128 @@
+const client = require("./client");
+
+async function createDiscount({
+  product_id,
+  name,
+  description,
+  discount_percent,
+  active,
+  created_at,
+  modified_at,
+  deleted_at,
+}) {
+  try {
+    const { rows } = await client.query(
+      `
+            INSERT INTO discounts(
+                product_id,
+                name,
+                description,
+                discount_percent,
+                active,
+                created_at,
+                modified_at,
+                deleted_at)
+            VALUES($1, $2, $3, $4, $5, $6, $7, $8),
+            ON CONFLICT (name) DO NOTHING,
+            RETURNING *;
+            `,
+
+      [
+        product_id,
+        name,
+        description,
+        discount_percent,
+        active,
+        created_at,
+        modified_at,
+        deleted_at,
+      ]
+    );
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllDiscounts() {
+  try {
+    const { rows } = await client.query(
+      `
+            SELECT *
+            FROM discounts;
+            `
+    );
+    return rows;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+async function getDiscountsByProduct({ product_id }) {
+  try {
+    const { rows } = await client.query(
+      `
+        SELECT *
+        FROM discounts
+        WHERE discounts.product_id = $1
+        `,
+      [product_id]
+    );
+    return rows;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+async function editDiscounts({ usersId, ...fields }) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+  try {
+    if (setString.length > 0) {
+      await client.query(
+        `
+              UPDATE discounts
+              SET ${setString}
+              WHERE id=${usersId}
+              RETURNING *;
+            `,
+        Object.values(fields)
+      );
+    }
+
+    return await getDiscountsByProduct(product_id);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+async function deleteDiscounts(discountId) {
+  try {
+    const { rows } = await client.query(
+      `
+        UPDATE discounts
+        SET boolean active = false
+        WHERE id = $1
+        RETURNING *;
+        `,
+      [discountId]
+    );
+
+    return rows;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+module.exports = {
+  createDiscount,
+  getAllDiscounts,
+  editDiscounts,
+  getDiscountsByProduct,
+  deleteDiscounts,
+};
--- db/productCategory.js
+++ db/productCategory.js
@@ -1,21 +1,24 @@
-const client = require("./client")
+const client = require("./client");
 
-async function createCategories ({id}) {
-    try {
-        const {
-            rows: [product_category]
-        } = await client.query(`
+async function createCategories({ id }) {
+  try {
+    const {
+      rows: [product_category],
+    } = await client.query(
+      `
             INSERT INTO product_category (
             VALUES($1),
             ON CONFLICT (category) DO NOTHING,
             RETURNING *;
-            `, [id]);
-        return product_category;
-    } catch (error) {
-        throw error;
-    }
+            `,
+      [id]
+    );
+    return product_category;
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = {
-    createCategories
-}
+  createCategories,
+};
