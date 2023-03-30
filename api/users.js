0b82c12566bbd7ee22f1530c4721ddf71897daaa
ziggyboy2001 <brentpurks1@icloud.com>
2023-03-30 13:50:05 -0500

    usersAPI and users_paymentsAPI

---
+++ api/users.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const { getUsersByUsername, createUser } = require("../db");
+const { requireUser } = require("./utils");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+
+//POST api/users/register
+router.post("/register", async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const _user = await getUsersByUsername(username);
+
+    if (_user) {
+      next({
+        name: "UserExistsError",
+        message: `User ${username} is already taken.`,
+      });
+    }
+
+    if (password.length < 8) {
+      next({
+        name: "PasswordError",
+        message: "Password Too Short!",
+      });
+    }
+    const user = await createUser({
+      username,
+      password,
+    });
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1w",
+      }
+    );
+
+    res.send({
+      message: "thank you for signing up",
+      token,
+      user,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+//POST /api/users/login
+router.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password to login",
+    });
+  }
+
+  try {
+    const user = await getUsersByUsername(username);
+    if (user && user.password == password) {
+      const id = user.id;
+      const username = user.username;
+      const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+        expiresIn: "168h",
+      });
+
+      res.send({ message: "you're logged in!", token, user });
+    } else {
+      next({
+        name: "InvalidCredentialsError",
+        message: "Username or password is incorrect",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+//GET /api/users/me
+
+router.get("/me", requireUser, async (req, res, next) => {
+  try {
+    res.send(req.user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
---
+++ api/users_payments.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const {
+  getPaymentById,
+  patchPayment,
+  createPayment,
+  deletePayment,
+  getPaymentByUser,
+} = require("../db");
+const { requireUser } = require("./utils");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+
+// GET /api/user_payments
+router.get("/", async (req, res, next) => {
+  try {
+    const getPayments = await getPaymentByUser(userId);
+    const payment = getPayments.filter((payment) => {
+      return payment;
+    });
+
+    res.send(payment);
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+// POST /api/users_payments
+router.post("/", requireUser, async (req, res, next) => {
+  const { paymentType, provider, accountNo = "" } = req.body;
+
+  const paymentData = {
+    paymentId: req.user.id,
+    paymentType,
+    provider,
+    accountNo,
+  };
+
+  try {
+    const post = await createPayment(paymentData);
+    if (post) {
+      res.send(post);
+    } else {
+      next({
+        name: "PaymentCreationError",
+        message: "Error creating payment.",
+      });
+    }
+  } catch ({ name, message }) {
+    next({
+      name,
+      message: `An payment with ID ${paymentData.paymentId} already exists`,
+    });
+  }
+});
+
+// PATCH /api/users_payments/:paymentId
+router.patch("/:paymentId", requireUser, async (req, res, next) => {
+  try {
+    const { paymentId } = req.params;
+    const getPaymentId = await getPaymentById(paymentId);
+    if (!getPaymentId) {
+      next({
+        name: "PaymentNotFound",
+        message: `Payment ${paymentId} not found`,
+      });
+    } else {
+      const { paymentType, provider, accountNo } = req.body;
+      try {
+        const updatedPayment = await patchPayment({
+          id: paymentId,
+          paymentType,
+          provider,
+          accountNo,
+        });
+        res.send(updatedPayment);
+      } catch (error) {
+        next({
+          name: "",
+          message: `An payment with ID ${paymentId} already exists`,
+        });
+        console.log(error);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// DELETE /api/users_payments/:paymentId
+
+router.delete("/:paymentId", requireUser, async (req, res, next) => {
+  try {
+    const { paymentId } = req.params;
+    const payment = await getPaymentById(paymentId);
+
+    if (payment && payment.userId === req.user.id) {
+      await deletePayment(paymentId);
+
+      res.send(payment);
+    } else {
+      res.status(403);
+      next(
+        payment
+          ? {
+              name: "UnauthorizedUserError",
+              message: `User ${req.user.username} is not allowed to delete payment with ID ${paymentId}`,
+            }
+          : {
+              name: "paymentNotFoundError",
+              message: `User ${req.user.username} is not allowed to delete payment with ID ${paymentId}`,
+            }
+      );
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+module.exports = router;
