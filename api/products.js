7250812d9c31c93e31c3f5457e02fc1daf84dd7e
dylanstewart06 <dylanstewart06@gmail.com>
2023-03-31 08:35:11 -0600

    products api finished

--- api/products.js
+++ api/products.js
@@ -61,9 +61,9 @@ router.get("/:productId", async (req, res, next) => {
 });
 
 router.patch("/:productId", async (req, res, next) => {
-  const productData = { 
+  const productData = {
     creatorId: req.user.id,
-    categoryId: req.body.isPublic,
+    categoryId: req.body.categoryId,
     name: req.body.name,
     description: req.body.description,
     price: req.body.price,
@@ -71,36 +71,68 @@ router.patch("/:productId", async (req, res, next) => {
     imgURL: req.body.imgURL,
   };
   try {
-    const editedProduct = await getProductById(productData);
     if (product.creatorId === req.user.id) {
       const result = await editProduct(productData);
-      res.send(editedProduct);
+      res.send(result);
     }
   } catch (error) {
-    next ({
+    next({
       name: "updateError",
-      message: "Could not update product"
+      message: "Could not update product",
     });
   }
 });
 
-router.delete("/:productId" , async (req, res, next) => {
+router.delete("/:productId", async (req, res, next) => {
   try {
-    const {productId} = req.params;
+    const { productId } = req.params;
     const product = await getProductById(productId);
     if (!product) {
-      next ({
+      next({
         name: "Deletion Error",
-        message: "Error deleting product"
+        message: "Error deleting product",
       });
     } else {
       const deletedProduct = await deleteProducts(productId);
-      res.send(deletedProduct)
+      res.send(deletedProduct);
     }
-  } catch ({name, message}) {
-    next ({ name, message});
+  } catch ({ name, message }) {
+    next({ name, message });
   }
-})
+});
 
+router.get("/products", async (req, res, next) => {
+  const userId = req.user.id;
+  try {
+    const product = await getProductsByUser(userId);
+
+    if (!product) {
+      next({
+        name: "DoesNotExist",
+        message: `product not found`,
+      });
+    } else {
+      res.send(product);
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+router.get("/categoryId", async (req, res, next) => {
+  const categoryId = req.user.id;
+  try {
+    const category = await getProductsByCategory(categoryId);
+
+    if (!category) {
+      next({
+        name: "DoesNotExist",
+        message: `product not found`,
+      });
+    } else {
+      res.send(category);
+    }
+  } catch (error) {}
+});
 
 module.export = router;
