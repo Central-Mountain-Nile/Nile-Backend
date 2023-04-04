7af4b3dd84a5af495c7cb73d180288dd6f4bd88c
MonkComputer <bmonk1996@yahoo.com>
2023-04-04 12:17:24 -0500

    fixing is admin

--- api/admin.js
+++ api/admin.js
@@ -1,9 +1,27 @@
 const express = require("express");
 
 const { requireUser, requireAdmin } = require("./utils");
-const { getUsersById, getPaymentByUser, getOrdersByUser } = require("../db");
+const {
+  getUsersById,
+  getPaymentByUser,
+  getOrdersByUser,
+  makeAdmin,
+} = require("../db");
 
 const adminRouter = express.Router();
+adminRouter.patch("/users", requireUser, async (req, res, next) => {
+  const secretCode = req.body.secretCode;
+  try {
+    if (secretCode === process.env.SECRET_CODE) {
+      const user = await makeAdmin(req.user.id, secretCode);
+      res.send(user);
+    } else {
+      next({ name: "adminError", message: "invalid admin password" });
+    }
+  } catch (error) {
+    throw error;
+  }
+});
 
 adminRouter.get("/users", requireUser, requireAdmin, async (req, res, next) => {
   try {
@@ -41,34 +59,40 @@ adminRouter.patch(
     const { productId } = req.params;
     const productData = { ...req.body };
     try {
-        const product = await getProductById(productId);
-        if(!product){
-            next({name:"adminError",message:`product ${productId} des not exist`})
-        }
-        const result = await editProduct({ id: productId, ...productData });
-        res.send(result);
+      const product = await getProductById(productId);
+      if (!product) {
+        next({
+          name: "adminError",
+          message: `product ${productId} des not exist`,
+        });
+      }
+      const result = await editProduct({ id: productId, ...productData });
+      res.send(result);
     } catch (error) {
       next(error);
     }
   }
 );
 adminRouter.delete(
-    "/product/:productId",
-    requireUser,
-    requireAdmin,
-    async (req, res, next) => {
-      const { productId } = req.params;
-      try {
-          const product = await getProductById(productId);
-          if(!product){
-              next({name:"adminError",message:`product ${productId} des not exist`})
-          }
-          const deletedProduct = await deleteProducts(productId);
-          res.send(deletedProduct);
-      } catch (error) {
-        next(error);
+  "/product/:productId",
+  requireUser,
+  requireAdmin,
+  async (req, res, next) => {
+    const { productId } = req.params;
+    try {
+      const product = await getProductById(productId);
+      if (!product) {
+        next({
+          name: "adminError",
+          message: `product ${productId} des not exist`,
+        });
       }
+      const deletedProduct = await deleteProducts(productId);
+      res.send(deletedProduct);
+    } catch (error) {
+      next(error);
     }
-  );
+  }
+);
 
 module.exports = adminRouter;
