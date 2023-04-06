9b0a55280c79f8e945383d6a065e8dcf2c0c56e9
ziggyboy2001 <brentpurks1@icloud.com>
2023-04-06 09:33:30 -0500

    adding stripe

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 const apiRouter = require("./api/index");
 const morgan = require("morgan");
 const cors = require("cors");
+const { resolve } = require("path");
+
+// const env = require("dotenv").config({ path: "./.env" });
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +14,44 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api", apiRouter);
+
+app.use(express.static(process.env.STATIC_DIR));
+
+app.get("/", (req, res) => {
+  const path = resolve(process.env.STATIC_DIR + "/index.html");
+  res.sendFile(path);
+});
+
+app.get("/config", (req, res) => {
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: "USD",
+      amount: 1999,
+      automatic_payment_methods: { enabled: true },
+    });
+
+    // Send publishable key and PaymentIntent details to client
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (e) {
+    return res.status(400).send({
+      error: {
+        message: e.message,
+      },
+    });
+  }
+});
+
+app.listen(5252, () =>
+  console.log(`Node server listening at http://localhost:5252`)
+);
 
 app.get("*", (req, res) => {
   res.status(404).send({
