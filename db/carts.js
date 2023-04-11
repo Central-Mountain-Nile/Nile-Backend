302525a65a6b6f15979cbcf20e070845bfdf7593
MonkComputer <bmonk1996@yahoo.com>
2023-04-11 14:28:11 -0500

    small change

--- db/carts.js
+++ db/carts.js
@@ -38,7 +38,7 @@ async function getCartItems(userId) {
       rows,
     } = await client.query(
       `
-      SELECT carts.*, cart_items.*, products.quantity AS "remainingQuantity", products.price, products.active
+      SELECT carts.*, cart_items.*, products.quantity AS "remainingQuantity", products.price, products.active, products."imgURL"
       FROM carts
       JOIN cart_items 
       ON carts.id=cart_items."cartId"
